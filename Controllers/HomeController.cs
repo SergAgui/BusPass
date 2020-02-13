8014e705cb2ccf95d6fdd9ed244db4ce5dc89401
Sergio <Sergeagui@gmail.com>
2020-02-13 11:33:18 -0600

    new views and updated HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,21 @@ namespace BusPass.Controllers
             return View();
         }
 
+        public IActionResult Alerts()
+        {
+            return View();
+        }
+
+        public IActionResult Fares()
+        {
+            return View();
+        }
+
+        public IActionResult Purchase()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
