2664113683b9dee8d5450e409429b20b8a34faa1
Sergio <Aguilar>
2021-03-11 09:48:41 -0600

    added repository interface

---
+++ Models/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusPass.Models
+{
+    public interface IRepository
+    {
+        CustomerModel FindCustomerId(int id);
+        void NewCustomer(CustomerModel customer);
+        void RemoveCustomer(int id);
+    }
+}
--- Models/Repository.cs
+++ Models/Repository.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 
 namespace BusPass.Models
 {
-    public class Repository
+    public class Repository : IRepository
     {
         private readonly ApplicationDbContext _context;
         public Repository(ApplicationDbContext context) => _context = context;
 
-        public CustomerModel GetCustomerId(int id)
+        //Find Customer by Id
+        public CustomerModel FindCustomerId(int id)
         {
             var customer = _context.Customers.Where(cust => cust.Id == id).FirstOrDefault();
+            return customer;
         }
 
         //Add new customers to db
@@ -26,9 +28,10 @@ namespace BusPass.Models
         //Remove customer acct from db
         public void RemoveCustomer(int custId)
         {
-            var customer = GetCustomerId(custId);
+            var customer = FindCustomerId(custId);
             _context.Customers.Remove(customer);
             _context.SaveChanges();
         }
+
     }
 }
