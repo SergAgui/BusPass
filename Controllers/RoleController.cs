77ef6370bbdfdb52b0e0a9a3e99877f6b04df346
Sergeagui@gmail.com <sergeagui@gmail.com>
2021-04-23 11:13:49 -0500

    Added Role View folder

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusPass.Controllers
+{
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+        public RoleController(RoleManager<IdentityRole> roleMan) => roleManager = roleMan;
+
+        // GET: RoleController
+        public ActionResult Index()
+        {
+            return View(roleManager.Roles);
+        }
+
+        // GET: RoleController/Create
+        public ActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        // POST: RoleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+            /*try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }*/
+        }
+
+        // GET: RoleController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RoleController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace BusPass
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("BusPassDb")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
             services.AddTransient<IRepository, Repository>();
