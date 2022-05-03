9b7751abade2a80922fbf1c9ea016bf361c6c47e
Sergeagui@gmail.com <sergeagui@gmail.com>
2022-05-03 15:04:16 -0500

    Removed JS until later

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,14 +1,4 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-function orderExists() {
-    let text = "Order already exists! Are you sure you want to continue?";
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 0 && this.status == 200) {
-            if (confirm(text) == false) {
-                xhr.abort();
-            }
-        }
-    }
-}
+// Write your JavaScript code.
